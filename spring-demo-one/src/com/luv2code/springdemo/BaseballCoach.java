0e758079f427328da3c18b1d77176ed0289d2b3e
FireOnIcecube <z0388848@gmail.com>
2023-01-19 00:39:11 +0800

    5: Spring Dependency Injection - XML Configuration 6/15

--- spring-demo-one/src/com/luv2code/springdemo/BaseballCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/BaseballCoach.java
@@ -2,8 +2,22 @@ package com.luv2code.springdemo;
 
 public class BaseballCoach implements Coach {
 	
+	
+	private FortuneService fortuneService;
+	
+	public BaseballCoach(FortuneService theFortuneService) {
+		fortuneService =theFortuneService;
+	}
+	
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Spend 30 minutes on battle practice";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return  fortuneService.getFortune(); 
 	}
 }
